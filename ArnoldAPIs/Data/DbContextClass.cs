b42c1a84d0feb9955775863aaa925be48fd97b75
Curtis Arnold <39891074+iceEngineered@users.noreply.github.com>
2024-03-28 19:51:06 -0400

    Update DbContextClass.cs
    Added chatGPT prompts used

--- ArnoldAPIs/Data/DbContextClass.cs
+++ ArnoldAPIs/Data/DbContextClass.cs
@@ -15,6 +15,17 @@ public class DbContextClass : DbContext
 
         modelBuilder.Entity<Disaster>()
             .HasKey(ndh => new { ndh.CID, ndh.NDDate });
+
+        /*since the NaturalDisasterHistory table has a composite key (otherwise a city or date wouldn't be able to have multiple disasters stored)
+        I had to use ChatGPT to fix this after getting an error (some variable names were changed after the prompt was given)
+
+       Prompt 1:  System.InvalidOperationException: 'The entity type 'NaturalDisasterHistory' has multiple properties with the [Key] attribute. Composite primary keys configured by placing the [PrimaryKey] attribute on the entity type class, or by using 'HasKey' in 'OnModelCreating'.'          
+        
+        Prompt 2: I want DateOccured to also be part of the key
+
+        That output the protected override
+
+        */
     }
 }
 
