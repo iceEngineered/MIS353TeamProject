7a746a16db391c0805043a9bfd6e79a9c8b0f169
Curtis Arnold <39891074+iceEngineered@users.noreply.github.com>
2024-03-28 19:11:55 -0400

    Add files via upload

---
+++ ArnoldAPIs/Controllers/DisasterController.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using MISTeamProjectAPI.Repositories;
+using MISTeamProjectAPI.Entities;
+using Microsoft.Extensions.Hosting;
+
+namespace MISTeamProjectAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class DisasterController : ControllerBase
+    {
+        private readonly IntDisasterService _DisasterService;
+        public DisasterController(IntDisasterService DisasterService)
+        {
+            _DisasterService = DisasterService;
+        }
+        [HttpGet("{CID}")]
+        public async Task<ActionResult<List<Disaster>>> GetCityDisasters(int CID)
+        {
+            var disasterDetails = await _DisasterService.GetCityDisasters(CID);
+            if (disasterDetails == null)
+            {
+                return NotFound();
+            }
+            return disasterDetails;
+        }
+
+        [HttpPost]
+        public async Task<ActionResult<int>> AddDisaster(Disaster disaster)
+        {
+            var DisasterDetails = await _DisasterService.AddDisaster(disaster);
+            if (DisasterDetails == 0)
+            {
+                // return NotFound();
+            }
+            return DisasterDetails;
+        }
+    }
+}
+
---
+++ ArnoldAPIs/Entities/Disaster.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace MISTeamProjectAPI.Entities
+{
+    public partial class Disaster {
+
+        
+     public int CID { get; set; }
+        
+     public string NDType { get; set; }
+
+    public DateOnly NDDate { get; set; }
+        
+
+    }
+}
