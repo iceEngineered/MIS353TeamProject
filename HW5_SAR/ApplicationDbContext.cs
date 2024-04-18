97c4dc640c75aaa4740452b7b5b6c88936d27022
Skylar Robinson <142524901+sar00019@users.noreply.github.com>
2024-04-18 13:50:20 -0400

    other files

---
+++ HW5_SAR/ApplicationDbContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace HW5_SAR.Data
+{
+    public class ApplicationDbContext : IdentityDbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+    }
+}
